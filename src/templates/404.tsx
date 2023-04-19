62f1a78b947720463469b90f8bcf6264595df1b1
Aaron Pavlick <apavlick@yext.com>
2023-04-19 14:49:23 -0400

    404 page

---
+++ src/templates/404.tsx
@@ -0,0 +1,75 @@
+import {
+  TemplateProps,
+  TemplateRenderProps,
+  GetHeadConfig,
+  GetPath,
+  Template,
+  HeadConfig,
+} from "@yext/pages";
+import * as React from "react";
+import Main from "../layouts/Main";
+import "../index.css";
+import Header from "../components/Header";
+import skiSign from "../assets/images/ski-sign.avif";
+
+export const getPath: GetPath<TemplateProps> = () => {
+  return "404.html";
+};
+
+export const getHeadConfig: GetHeadConfig<
+  TemplateRenderProps
+> = (): HeadConfig => {
+  return {
+    title: "Page Not Found",
+    charset: "UTF-8",
+    viewport: "width=device-width, initial-scale=1",
+  };
+};
+
+const FourOhFour: Template<TemplateRenderProps> = ({
+  document,
+}: TemplateRenderProps) => {
+  const { _site } = document;
+
+  return (
+    <Main>
+      <div className="relative">
+        <Header directory={_site} />
+      </div>
+      <div className="h-[calc(100vh-64px)]">
+        <div className="grid min-h-full grid-cols-1 grid-rows-[1fr,auto,1fr] bg-white lg:grid-cols-[max(50%,36rem),1fr]">
+          <main className="mx-auto w-full max-w-7xl px-6 py-24 sm:py-32 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:px-8">
+            <div className="max-w-lg">
+              <p className="text-base font-semibold leading-8 text-gray-900">
+                404
+              </p>
+              <h1 className="mt-4 text-3xl font-bold tracking-tight text-sky-400 sm:text-5xl">
+                Page not found
+              </h1>
+              <p className="mt-6 text-base leading-7 text-gray-600">
+                Sorry, we couldn’t find the page you’re looking for.
+              </p>
+              <div className="mt-10">
+                <a
+                  href="/"
+                  className="text-sm font-semibold leading-7 text-sky-400"
+                >
+                  <span aria-hidden="true">&larr;</span> Back to home
+                </a>
+              </div>
+            </div>
+          </main>
+          <div className="hidden lg:relative lg:col-start-2 lg:row-start-1 lg:row-end-4 lg:block">
+            <img
+              className="absolute inset-0 h-full w-full object-cover"
+              src={skiSign}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </Main>
+  );
+};
+
+export default FourOhFour;
